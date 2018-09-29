9db65f4e93c13fba0d4fde19a5afdee51ab2fae1
Bruno Medeiros <bmdefreitas@gmail.com>
2018-09-28 23:56:17 -0300

    Iniciando o projeto com Spring Boot

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# desafio-proxy
-Desafio Proxy Reverso - JAVA
+# Proxy Reverso
+Proxy HTTPS Reverso - JAVA com suporte SNI
+
+
+
+# Funcionamento da Solução
+
+![alt text](https://raw.githubusercontent.com/bmdefreitas/desafio-proxy/master/Funcionamento.jpg)
+
---
+++ src/main/java/br/com/bmdefreitas/proxy/proxyreverso/ProxyReversoApplication.java
@@ -0,0 +1,12 @@
+package br.com.bmdefreitas.proxy.proxyreverso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyReversoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyReversoApplication.class, args);
+	}
+}
