8b9b1f672e7811c199219369c24be71ba57abc77
Bruno Medeiros <bmdefreitas@gmail.com>
2018-10-05 01:30:48 -0300

    Tomcat Connector - Configurações
    Tentativa de configurar os certificados para cada FQDN. Ainda não concluído.

--- src/main/java/br/com/bmdefreitas/proxy/proxyreverso/ConnectorConfig.java
+++ src/main/java/br/com/bmdefreitas/proxy/proxyreverso/ConnectorConfig.java
@@ -5,6 +5,7 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.apache.tomcat.util.net.SSLHostConfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,6 +36,25 @@ public class ConnectorConfig {
 		connector.setPort(8080);
 		connector.setSecure(false);
 		connector.setRedirectPort(8443);
+		
+		SSLHostConfig sslHostConfig1 = new SSLHostConfig();
+		sslHostConfig1.setHostName("test1.localdomain");
+		sslHostConfig1.setCertificateKeyAlias("test1.localdomain");
+		sslHostConfig1.setCertificateKeystoreFile("keystore.p12");
+		sslHostConfig1.setCertificateKeystorePassword("123456");
+		sslHostConfig1.setCertificateKeystoreType("PKCS12");
+		
+		connector.addSslHostConfig(sslHostConfig1);
+		
+		SSLHostConfig sslHostConfig2= new SSLHostConfig();
+		sslHostConfig2.setHostName("test2.localdomain");
+		sslHostConfig2.setCertificateKeyAlias("test2.localdomain");
+		sslHostConfig2.setCertificateKeystoreFile("keystore.p12");
+		sslHostConfig2.setCertificateKeystorePassword("123456");
+		sslHostConfig2.setCertificateKeystoreType("PKCS12");
+		
+		connector.addSslHostConfig(sslHostConfig2);
+		
 		return connector;
 	}
 }
