a109ac0f22dc7c407bcefa69d91a79f131ea6629
Bruno Medeiros <bmdefreitas@gmail.com>
2018-10-06 22:24:42 -0300

    Configurando Tomcat com o KeyStore
    Configurando o Tomcat. A partir de agora cada Host responde com seus certificados correpondentes.

--- src/main/java/br/com/bmdefreitas/proxy/proxyreverso/ConnectorConfig.java
+++ src/main/java/br/com/bmdefreitas/proxy/proxyreverso/ConnectorConfig.java
@@ -1,60 +1,64 @@
 package br.com.bmdefreitas.proxy.proxyreverso;
 
 
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.apache.tomcat.util.net.SSLHostConfig;
+import org.apache.tomcat.util.net.SSLHostConfigCertificate;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectorConfig {
-
+	
 	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
-	    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-	        
-	        protected void postProcessContext(Context context) {
-	            SecurityConstraint securityConstraint = new SecurityConstraint();
-	            securityConstraint.setUserConstraint("CONFIDENTIAL");
-	            SecurityCollection collection = new SecurityCollection();
-	            collection.addPattern("/*");
-	            securityConstraint.addCollection(collection);
-	            context.addConstraint(securityConstraint);
-	        }
-	    };
-	    tomcat.addAdditionalTomcatConnectors(getHttpConnector());
-	    return tomcat;
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createSslConnector());
+		return tomcat;
 	}
 
-	private Connector getHttpConnector() {
+	private Connector createSslConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(8443);
-		
-		SSLHostConfig sslHostConfig1 = new SSLHostConfig();
-		sslHostConfig1.setHostName("test1.localdomain");
-		sslHostConfig1.setCertificateKeyAlias("test1.localdomain");
-		sslHostConfig1.setCertificateKeystoreFile("keystore.p12");
-		sslHostConfig1.setCertificateKeystorePassword("123456");
-		sslHostConfig1.setCertificateKeystoreType("PKCS12");
-		
-		connector.addSslHostConfig(sslHostConfig1);
-		
-		SSLHostConfig sslHostConfig2= new SSLHostConfig();
-		sslHostConfig2.setHostName("test2.localdomain");
-		sslHostConfig2.setCertificateKeyAlias("test2.localdomain");
-		sslHostConfig2.setCertificateKeystoreFile("keystore.p12");
-		sslHostConfig2.setCertificateKeystorePassword("123456");
-		sslHostConfig2.setCertificateKeystoreType("PKCS12");
-		
-		connector.addSslHostConfig(sslHostConfig2);
-		
-		return connector;
+		Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+		try {
+			connector.setScheme("https");
+			connector.setSecure(true);
+			connector.setPort(8443);
+			protocol.setSSLEnabled(true);
+			protocol.setKeystoreFile("/home/bruno/git/desafio-proxy/keystore.p12");
+			protocol.setKeystorePass("123456");
+			protocol.setKeyAlias("localdomain");
+			
+			SSLHostConfig sslHostConfig1 = new SSLHostConfig();	
+			sslHostConfig1.setHostName("test1.localdomain");
+			
+			SSLHostConfigCertificate certificate1 = new SSLHostConfigCertificate(sslHostConfig1, SSLHostConfigCertificate.DEFAULT_TYPE);		
+			certificate1.setCertificateKeystoreFile("/home/bruno/git/desafio-proxy/keystore.p12");
+			certificate1.setCertificateKeystorePassword("123456");
+			certificate1.setCertificateKeyAlias("test1.localdomain");		
+			certificate1.setCertificateKeystoreType("PKCS12");
+			sslHostConfig1.addCertificate(certificate1);
+			
+			connector.addSslHostConfig(sslHostConfig1);
+			
+			SSLHostConfig sslHostConfig2 = new SSLHostConfig();	
+			sslHostConfig2.setHostName("test2.localdomain");
+
+			SSLHostConfigCertificate certificate2 = new SSLHostConfigCertificate(sslHostConfig1, SSLHostConfigCertificate.DEFAULT_TYPE);		
+			certificate2.setCertificateKeystoreFile("/home/bruno/git/desafio-proxy/keystore.p12");
+			certificate2.setCertificateKeystorePassword("123456");
+			certificate2.setCertificateKeyAlias("test2.localdomain");		
+			certificate2.setCertificateKeystoreType("PKCS12");
+			sslHostConfig2.addCertificate(certificate2);
+			
+			connector.addSslHostConfig(sslHostConfig2);
+			return connector;
+		}
+		catch (Exception ex) {
+			throw new IllegalStateException("Error:", ex);
+		}
 	}
 }
